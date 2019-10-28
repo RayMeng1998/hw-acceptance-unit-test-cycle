c7d2fb7a5b911efad42b93b4b78e4a7497428c1d
Ziyue Meng <z.y.meng@berkeley.edu>
2019-10-27 20:00:38 -0700

    Update routes.rb

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
+  get 'similar_movies/:title' => 'movies#search', as: :search_similar_movies
   root :to => redirect('/movies')
 end
